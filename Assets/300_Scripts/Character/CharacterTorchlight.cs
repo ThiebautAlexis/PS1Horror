254d94aa1d9814c4c08973a7a1025be3c447dd57
Alexis <thiebautalexis01@gmail.com>
2022-05-31 12:39:26 +0200

    Packages divers et variés

---
+++ Assets/300_Scripts/Character/CharacterTorchlight.cs
@@ -0,0 +1,17 @@
+using EnhancedEditor;
+using System;
+using UnityEngine;
+
+namespace HorrorPS1
+{
+    public class CharacterTorchlight : MonoBehaviour
+    {
+        #region Fields and Properties
+        [SerializeField, Enhanced, Required] private new Light light = null;
+        #endregion
+
+        #region Methods 
+
+        #endregion
+    }
+}
